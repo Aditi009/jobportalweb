57e6a02bd26a0255af09a7f20ebf2e098b8a9f2d
Aditissdn <Aditi009@users.noreply.github.com>
2020-07-19 13:28:37 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="https://kit.fontawesome.com/yourcode.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    
+        <nav class="navbar navbar-expand-lg navbar-dark " style="    background-color:rgba(20, 177, 177, 0.89);">
+            <a class="navbar-brand" href="#"><img src="img (2).png" height="60px";width="100px"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav" >
+                <a class="nav-item nav-link active"  id="navlink" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link active" id="navlink" href="#">About</a>
+                <a class="nav-item nav-link active" id="navlink" href="#">Contect</a>
+                <a class="nav-item nav-link active" id="navlink" href="#">Job Alert</a>
+               
+              </div>
+
+            
+            </div>
+            
+          </nav>
+
+          <div class="container-fluid">
+
+<div class="row">
+    
+
+    <div class="col-sm-12" id="front">
+      <button type="button" class="btn btn-outline-light" "> Sign up</button>
+        <h1 style="margin-top: 10px;margin-left: 60px;">JOB AROUND YOU <img src="logo.png" height="60px";width="100px"></h1>
+        <hr style="background-color: rgb(216, 205, 205);margin-left: 40px;width: 500px;">
+       <pre style="color: rgb(168, 165, 165);font-family:Arial, Helvetica, sans-serif">             Networking. A large number of job vacancies aren't 
+            actually advertised, so in order to find out about these vacancies, 
+            job seekers must find a way to get their foot in the door
+     </pre>
+    </div>
+</div>
+
+ 
+
+<div class="row" style="padding: 10px;">
+
+    <div class="col-sm-11 ml-5 xs-11 mr-5" id="between" >
+
+        <form>
+            <div class="form-row mt-5 ml-5  mr-5" >
+              <div class="col-sm-3 xs-12 xs-mt-2 mb-2">
+                <input type="text" class="form-control mt-2" placeholder="Search Keyword">
+              </div>
+              <div class="col-sm-3 xs-12 xs-mt-2 mb-2">
+                <input type="text" class="form-control mt-2" placeholder="Company">
+              </div>
+              <div class="col-sm-3 mb-2">
+                <input type="text" class="form-control mt-2" placeholder="Location">
+              </div>
+              <div class="col-sm-3 mb-2">
+                <button type="button" class="btn btn mt-2" style="background-color: rgb(10, 112, 102);color: white;">Find Best Job</button>
+              </div>
+            </div>
+          </form>
+
+    </div>
+</div>
+
+<h1 style="color: black;text-align: center;margin-top: 100PX;font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif ;"><img src="joblogo.png" style="height:60px;width:60px;align-items: center;">TOP RECRUITERS</h1>
+<hr style="background-color: rgb(10, 9, 9);margin-left: 600px;width: 600px;margin-top: -10px;">
+<div class="row">
+
+
+   
+
+        <img src="top.png" class="col-sm-11">
+
+   
+</div>
+<h1 style="color: black;text-align: center;margin-top: 100PX;font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif ;"><img src="img (2).png" style="height:100px;width:100px;align-items: center;">RECENT JOB</h1>
+<hr style="background-color: rgb(10, 9, 9);margin-left: 600px;width: 600px;margin-top: -10px;">
+
+<div class="row ml-5 mt-5">
+<div class="col-sm-9 xs-6 lg-6 xm-6 media ml-5 mr-5" style="border-bottom: 10px solid rgba(20, 177, 177, 0.89);padding: 10px;box-shadow: 2px 2px 10px 1px rgba(20, 177, 177, 0.89);">
+  <img src="download.png" class="mr-3" alt="..." style="height:150px;width: 150px;">
+  <div class="media-body responsive">
+    
+    <h3>Python WEb development(Django)</h3>
+    <h6>XYZ pvt limited</h6>
+    <p><i class="fa fa-map-marker" aria-hidden="true"></i> Banglore</p>
+    <p style="margin-top: -15px;"><i class="fa fa-inr" aria-hidden="true"></i> 12000-140000</p>
+    <p style="margin-top: -15px;"><i class="fa fa-archive" aria-hidden="true"></i>2 year-9 year</p>
+    <p style="margin-top: -15px;">SKill <i class="fa fa-angle-right" aria-hidden="true"></i>  Python <i class="fa fa-angle-right" aria-hidden="true"></i>  Java</p>
+    <button type="button" class="btn btn-primary"style="background-color: rgb(41, 175, 162);color: white;" >Apply Now</button>
+  </div>
+</div>
+<div class="col-sm-9 xs-9 media ml-5 mr-5 mt-5" style="border-bottom: 10px solid rgba(20, 177, 177, 0.89);padding: 10px;box-shadow: 2px 2px 10px 1px rgba(20, 177, 177, 0.89);">
+  <img src="download.png" class="mr-3" alt="..." style="height:150px;width: 150px;">
+  <div class="media-body responsive">  background-color:rgba(20, 177, 177, 0.89);
+    
+    <h3>Python WEb development(Django)</h3>
+    <h6>XYZ pvt limited</h6>
+    <p><i class="fa fa-map-marker" aria-hidden="true"></i> Banglore</p>
+    <p style="margin-top: -15px;"><i class="fa fa-inr" aria-hidden="true"></i> 12000-140000</p>
+    <p style="margin-top: -15px;"><i class="fa fa-archive" aria-hidden="true"></i>2 year-9 year</p>
+    <p style="margin-top: -15px;">SKill <i class="fa fa-angle-right" aria-hidden="true"></i>  Python <i class="fa fa-angle-right" aria-hidden="true"></i>  Java</p>
+    <button type="button" class="btn btn" style="background-color: rgb(41, 175, 162);color: white;">Apply now</button>
+  </div>
+</div>
+
+
+
+</div>
+<h1 style="color: black;text-align: center;margin-top: 100PX;font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif ;"><img src="jobclues.png" style="height:100px;width:100px;align-items: center;">JOBCULES SITE STATS</h1>
+<hr style="background-color: rgb(10, 9, 9);margin-left: 600px;width: 600px;margin-top: -10px;">
+<div class="row ml-5 mt-5">
+
+  <div class="col-sm-2 lg-2 xs-12 ml-5 mt-2" >
+    <div class="card" style="height:150px;box-shadow: 5px 5px 10px 1px  rgba(20, 177, 177, 0.89) ;">
+      <div class="card-body">
+        <h5 class="card-title ml-2 mt-2" style="text-align: center;"><i class="fa fa-users" aria-hidden="true" ></i>100k+</h5>
+       
+     <h6 style="text-align: center;">Job Seekers</h6>
+  
+        
+      </div>
+    </div>
+
+
+
+  </div>
+  
+  <div class="col-sm-2 lg-2 xs-12 ml-5 mt-2" >
+    <div class="card" style="height:150px;box-shadow: 5px 5px 10px 1px  rgba(20, 177, 177, 0.89) ;">
+      <div class="card-body">
+        <h5 class="card-title ml-2 mt-2" style="text-align: center;"><i class="fa fa-building" aria-hidden="true"></i>100k+</h5>
+       
+     <h6 style="text-align: center;">Companies</h6>
+  
+        
+      </div>
+    </div>
+
+
+
+  </div>
+  
+  <div class="col-sm-2 lg-2 xs-12 ml-5 mt-2" >
+    <div class="card" style="height:150px;box-shadow: 5px 5px 10px 1px  rgba(20, 177, 177, 0.89) ;">
+      <div class="card-body">
+        <h5 class="card-title ml-2 mt-2" style="text-align: center;"><i class="fa fa-hand-peace-o" aria-hidden="true"></i>100k+</h5>
+       
+     <h6 style="text-align: center;">Active Job</h6>
+  
+        
+      </div>
+    </div>
+
+
+
+  </div>
+  
+  <div class="col-sm-2 lg-2 xs-12 ml-5 mt-2" >
+    <div class="card" style="height:150px;box-shadow: 5px 5px 10px 1px  rgba(20, 177, 177, 0.89) ;">
+      <div class="card-body">
+        <h5 class="card-title ml-2 mt-2" style="text-align: center;"><i class="fa fa-male" aria-hidden="true"></i>100k+</h5>
+       
+     <h6 style="text-align: center;">Employee</h6>
+  
+        
+      </div>
+    </div>
+
+
+
+  </div>
+</div>
+
+
+
+<div class="row mt-5" id="footer">
+
+
+<div class="col-sm-4 lg-4 xs-12">
+    <h2 class="ml-5" style="color:white";>About us</h2>
+    <hr style="background-color:rgba(20, 177, 177, 0.89);width: 200px;margin-left: 5px;">
+    <div class="content ml-5">
+
+
+        <p style="color:white";>A job is a regular and official activity that you do, and receive money (a salary) for your activity. It is also called a profession or an occupation. “work” refers to general efforts and activities done to accomplish a goal. “Work” can be done both inside an official job and outside a job</p>
+        <div class="social">
+
+            <a href="#"><i class="fa fa-facebook" aria-hidden="true" style="color:white";></i></span></a>
+            <a href="#"><i class="fa fa-twitter" aria-hidden="true" style="color:white";></i></a>
+            <a href="#"><i class="fa fa-instagram" aria-hidden="true" style="color:white";></i></a>
+            <a href="#"><i class="fa fa-youtube-play" aria-hidden="true" style="color:white";></i></a>
+        </div>
+    </div>
+</div>
+<div class="col-sm-4">
+    <h2  class="ml-5" style="color:white;">Address</h2>
+    <hr style="background-color: rgba(20, 177, 177, 0.89);margin-left:-20px;width: 200px;">
+<div class="content ml-5">
+<div class="place">
+ <h6 style="color:white";><i class="fa fa-map-marker" aria-hidden="true" style="color:white";></i>   Banglore </h6> 
+</div>
+<div class="phone">
+    <h6 style="color:white;";><i class="fa fa-phone" aria-hidden="true" style="color:white";></i>  00-90-120-90 </h6> 
+   </div>
+   <div class="email">
+    <h6 style="color:white;";><i class="fa fa-envelope-o" aria-hidden="true" style="color:white";></i>  Banglore </h6> 
+   </div>
+
+</div>
+</div>
+<div class="col-sm-3 mr-5 mb-5 mt-2 ml-5" style="   background-color:rgba(20, 177, 177, 0.89);padding: 10px;"> 
+     <h2  class="ml-5" style="color:white";>Contect us</h2>
+    <div class="content ml-2" >
+
+    
+      
+        <form action="#">
+            <div class="email">
+                <div class="text" style="color:white";>Email *</div>
+                <input type="email" style="width:270px;">
+            </div>
+            <div class="msg">
+                <div class="text" style="color:white";>Message *</div>
+                <textarea rows= '2' cols="35" required></textarea>
+            </div>
+            <div class="btn">
+                
+             <button type="submit" style="padding: 5px;background-color: blue;border-radius: 10px;color: white;">Send</button>
+            </div>
+
+
+
+
+        </form>
+    </div>
+</div>
+
+
+    </div>
+
+    </div>
+    
+</body>
+</html>
